9737e7ddf0fdc1e3b03f2520d2ca6b3e9c78023a
wyclif-maitho <isaacmaitho14@gmail.com>
2023-03-09 14:07:44 +0300

    added react

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+        <h1>Maitho Blog</h1>
+         <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blogs</a>
+         </div>
+        </nav>
+    );
+   
+}
+ 
+export default Navbar;
